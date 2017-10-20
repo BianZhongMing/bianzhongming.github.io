adc0376ba6726c0e14655bda8949252a7f758b56
jrbianzhongming <jrbianzhongming@jd.com>
2017-10-20 11:57:06 +0800

    del

--- SQL Server/monitoring and Maintenance/Lock Object.sql
+++
@@ -1,5 +0,0 @@
---Lock Object
-select   request_session_id  spid,OBJECT_NAME(resource_associated_entity_id) tableName 
-from   sys.dm_tran_locks with(nolock) where resource_type='OBJECT'
-and OBJECT_NAME(resource_associated_entity_id)='mkt_hkequd'
-
