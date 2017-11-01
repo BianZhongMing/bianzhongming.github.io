9f567f2185a7d0004a15909fd35eca6dfa95d090
jrbianzhongming <jrbianzhongming@jd.com>
2017-11-01 17:35:05 +0800

    index add

--- SQLServer/KeyPoint&&Sample/【SAMPLE】sqlserver 多列Update.sql
+++ SQLServer/KeyPoint&&Sample/【SAMPLE】sqlserver 多列Update.sql
@@ -1,13 +1,13 @@
-use datayesdb
+use testdb
 
-update [dbo].[bond]  set UPDATE_BY=b.UPDATE_BY,UPDATE_TIME=b.UPDATE_TIME
-from [dbo].[bond]  a,[10.21.139.64,3306].[datayesdb].[dbo].[bond] b
+update [dbo].[testable]  set UPDATE_BY=b.UPDATE_BY,UPDATE_TIME=b.UPDATE_TIME
+from [dbo].[testable]  a,[10.10.10.10,3306].[testdb].[dbo].[testable] b
 where a.id=b.id --Ö÷¼ü
 and a.UPDATE_BY='[SP:sys_chk_exe_by_rul]'
 
-update [dbo].[bond]  set UPDATE_BY=NULL
+update [dbo].[testable]  set UPDATE_BY=NULL
 where UPDATE_BY='[SP:sys_chk_exe_by_rul]' 
 
 Ð£Ñé£º
-select  id,UPDATE_BY,UPDATE_TIME from [dbo].[bond] 
+select  id,UPDATE_BY,UPDATE_TIME from [dbo].[testable] 
 where UPDATE_BY='[SP:sys_chk_exe_by_rul]'  
